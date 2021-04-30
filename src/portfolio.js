fd169b511247c76ba6f337c495a357a8c7c6b5a9
cmd08010 <colleendunion@gmail.com>
2021-04-30 09:39:57 -0400

    update volunteer

--- src/portfolio.js
+++ src/portfolio.js
@@ -212,14 +212,14 @@ const experience = {
   title: "Experience",
   subtitle: "Work, Internship and Volunteership",
   description:
-    "I have many different work and volunteer experiences. From working ",
+    "I have a vast array of different work and volunteer experiences. From working at Prudential assisting with ",
   header_image_path: "experience.svg",
   sections: [
     {
       title: "Work",
       experiences: [
         {
-          title: "eService Support Team Member/Training Facilitator,",
+          title: "eService Support Team Member/Training Facilitator",
           company: "Prudential",
           company_url: "https://prudential.com/",
           logo_path: "prudential-7.svg",
@@ -290,67 +290,68 @@ const experience = {
         },
       ],
     },
-    // {
-    //   title: "Volunteerships",
-    //   experiences: [
-    //     {
-    //       title: "Google Explore ML Facilitator",
-    //       company: "Google",
-    //       company_url: "https://about.google/",
-    //       logo_path: "google_logo.png",
-    //       duration: "June 2019 - April 2020",
-    //       location: "Hyderabad, Telangana",
-    //       description:
-    //         "Explore Machine Learning (ML) is a Google-sponsored program for university students to get started with Machine Learning. The curriculum offers 3 tracks of ML Content (Beginner, Intermediate, Advanced) and relies on university student facilitators to train other students on campus and to build opensource projects under this program.",
-    //       color: "#4285F4",
-    //     },
-    //     {
-    //       title: "Microsoft Student Partner",
-    //       company: "Microsoft",
-    //       company_url: "https://www.microsoft.com/",
-    //       logo_path: "microsoft_logo.png",
-    //       duration: "Aug 2019 - May 2020",
-    //       location: "Hyderabad, Telangana",
-    //       description:
-    //         "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
-    //       color: "#D83B01",
-    //     },
-    //     {
-    //       title: "Mozilla Campus Captain",
-    //       company: "Mozilla",
-    //       company_url: "https://www.mozilla.org/",
-    //       logo_path: "mozilla_logo.png",
-    //       duration: "Oct 2019 - May 2020",
-    //       location: "Kurnool, Andhra Pradesh",
-    //       description:
-    //         "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
-    //       color: "#000000",
-    //     },
-    //     {
-    //       title: "Developer Students Club Member",
-    //       company: "DSC IIITDM Kurnool",
-    //       company_url:
-    //         "https://www.linkedin.com/company/developer-students-club-iiitdm-kurnool",
-    //       logo_path: "dsc_logo.png",
-    //       duration: "Jan 2018 - May 2020",
-    //       location: "Kurnool, Andhra Pradesh",
-    //       description:
-    //         "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
-    //       color: "#0C9D58",
-    //     },
-    //     {
-    //       title: "Developer Program Member",
-    //       company: "Github",
-    //       company_url: "https://github.com/",
-    //       logo_path: "github_logo.png",
-    //       duration: "July 2019 - PRESENT",
-    //       location: "Work From Home",
-    //       description:
-    //         "I am actively contributing to many opensource projects. I have contributed to projects of organisations like Tensorflow, Uber, Facebook, Google, Scikit-learn, Kiwix, Sympy, Python, NVLabs, Fossasia, Netrack, Keras etc. These contributions include bug fixes, feature requests and formulating proper documentation for project.",
-    //       color: "#181717",
-    //     },
-    //   ],
-    // },
+    {
+      title: "Volunteerships",
+      experiences: [
+        {
+          title: "World Human Bridge - Bolivia",
+          company: "World Human Bridge",
+          company_url:
+            "https://sites.google.com/a/human-bridge.org/human-bridge-bolivia/",
+          logo_path: "google_logo.png",
+          duration: "January 2011 - April 2011",
+          location: "Sipe Sipe, Bolivia",
+          description:
+            " • Developed and implemented English language coursework for all levels. • Instructed all levels of students, from adults to young children with various levels of English proficiency.",
+          color: "#4285F4",
+        },
+        //     {
+        //       title: "Microsoft Student Partner",
+        //       company: "Microsoft",
+        //       company_url: "https://www.microsoft.com/",
+        //       logo_path: "microsoft_logo.png",
+        //       duration: "Aug 2019 - May 2020",
+        //       location: "Hyderabad, Telangana",
+        //       description:
+        //         "Microsoft Student Partner is a program for university students to lead the awareness and use of Cloud especially Azure tools in the development of their projects and startups. Under this program, I have organised hands on workshops and seminars to teach Cloud Computing concepts to students.",
+        //       color: "#D83B01",
+        //     },
+        //     {
+        //       title: "Mozilla Campus Captain",
+        //       company: "Mozilla",
+        //       company_url: "https://www.mozilla.org/",
+        //       logo_path: "mozilla_logo.png",
+        //       duration: "Oct 2019 - May 2020",
+        //       location: "Kurnool, Andhra Pradesh",
+        //       description:
+        //         "My responsibility for this program was to create opensource environment in college and in the city. We have organised multiple hackathons on the problems collected by ordinary people from Kurnool city. We have build opensource community of our own college. The community is available at dsc_iiitdmk on github.",
+        //       color: "#000000",
+        //     },
+        //     {
+        //       title: "Developer Students Club Member",
+        //       company: "DSC IIITDM Kurnool",
+        //       company_url:
+        //         "https://www.linkedin.com/company/developer-students-club-iiitdm-kurnool",
+        //       logo_path: "dsc_logo.png",
+        //       duration: "Jan 2018 - May 2020",
+        //       location: "Kurnool, Andhra Pradesh",
+        //       description:
+        //         "We have well established developer club in college which is directly associated with Google Developers. We have developed many interdisciplinary projects under the membership of this club. We have organised workshops and activities on Android Application Development, Flutter and React JS.",
+        //       color: "#0C9D58",
+        //     },
+        //     {
+        //       title: "Developer Program Member",
+        //       company: "Github",
+        //       company_url: "https://github.com/",
+        //       logo_path: "github_logo.png",
+        //       duration: "July 2019 - PRESENT",
+        //       location: "Work From Home",
+        //       description:
+        //         "I am actively contributing to many opensource projects. I have contributed to projects of organisations like Tensorflow, Uber, Facebook, Google, Scikit-learn, Kiwix, Sympy, Python, NVLabs, Fossasia, Netrack, Keras etc. These contributions include bug fixes, feature requests and formulating proper documentation for project.",
+        //       color: "#181717",
+        //     },
+      ],
+    },
   ],
 };
 
